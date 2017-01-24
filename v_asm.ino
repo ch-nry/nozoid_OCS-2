ce0d736ea5ea5542dd5c52cc8d58fb48f71ae0b3
cyrille henry <ch@nozoid.com>
2017-01-24 17:37:29 +0100

    correct licence information

--- v_asm.ino
+++ v_asm.ino
@@ -1,3 +1,20 @@
+// --------------------------------------------------------------------------
+// This file is part of the OCS-2 firmware.
+//
+//    OCS-2 firmware is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    OCS-2 firmware is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with OCS-2 firmware.  If not, see <http://www.gnu.org/licenses/>.
+// --------------------------------------------------------------------------
+
 inline uint64_t m_u32xu32_u64(uint32_t in1, uint32_t in2) {
   // mul uint32_t with uint32_t,
   uint64 out;
