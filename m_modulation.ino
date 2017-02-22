4d6be534a4c79f65a666ee26fdcde170239e77e8
cyrille henry <ch@nozoid.com>
2017-02-22 14:24:04 +0100

    filter ext modulation

--- m_modulation.ino
+++ m_modulation.ino
@@ -17,14 +17,24 @@
 
 // uint32_t envelope_filter;
 
+int32_t ext1_filter, ext2_filter, ext3_filter;
+
 // external CV
 inline void update_ext() {
 uint32_t tmp;
 
   #ifdef EXT_ANA
-    modulation_data[mod_EXT1] = 3*(adc_value16[EXT_1]-(1<<15));
-    modulation_data[mod_EXT2] = 3*(adc_value16[EXT_2]-(1<<15));
-    modulation_data[mod_EXT3] = 3*(adc_value16[EXT_3]-(1<<15));
+    tmp = hysteresis16(adc_value16[EXT_1], ext1_filter);
+    ext1_filter = tmp;
+    modulation_data[mod_EXT1] = 3*(tmp-(1<<15));
+    
+    tmp = hysteresis16(adc_value16[EXT_2], ext2_filter);
+    ext2_filter = tmp;    
+    modulation_data[mod_EXT2] = 3*(tmp-(1<<15));
+    
+    tmp = hysteresis16(adc_value16[EXT_3], ext3_filter);
+    ext3_filter = tmp;
+    modulation_data[mod_EXT3] = 3*(tmp-(1<<15));
   #endif
   #ifdef EXT_MIDI
     modulation_data[mod_EXT1] = MIDI_PITCHWHEEL-(1<<15);
@@ -32,9 +42,17 @@ uint32_t tmp;
     modulation_data[mod_EXT3] = MIDI_CC2-(1<<15);
   #endif
   #ifdef EXT_AUTO
-    modulation_data[mod_EXT1] = (use_midi_pitchwheel == 0)? 3*(adc_value16[EXT_1]-(1<<15)):MIDI_PITCHWHEEL-(1<<15); // attention, les modulation peuvent etre superieur a 16 bit
-    modulation_data[mod_EXT2] = (use_midi_mod_wheel == 0)?  3*(adc_value16[EXT_2]-(1<<15)):MIDI_MODWHEEL-(1<<15);
-    modulation_data[mod_EXT3] = (use_midi_cc2 == 0)?        3*(adc_value16[EXT_3]-(1<<15)):MIDI_CC2-(1<<15);
+    tmp = hysteresis16(adc_value16[EXT_1], ext1_filter);
+    ext1_filter = tmp;
+    modulation_data[mod_EXT1] = (use_midi_pitchwheel == 0)? 3*(tmp-(1<<15)):MIDI_PITCHWHEEL-(1<<15); // attention, les modulation peuvent etre superieur a 16 bit
+    
+    tmp = hysteresis16(adc_value16[EXT_2], ext2_filter);
+    ext2_filter = tmp;    
+    modulation_data[mod_EXT2] = (use_midi_mod_wheel == 0)?  3*(tmp-(1<<15)):MIDI_MODWHEEL-(1<<15);
+    
+    tmp = hysteresis16(adc_value16[EXT_3], ext3_filter);
+    ext3_filter = tmp;
+    modulation_data[mod_EXT3] = (use_midi_cc2 == 0)?        3*(tmp-(1<<15)):MIDI_CC2-(1<<15);
   #endif
   
   tmp = 0xFFFF-adc_value16[LDR];
